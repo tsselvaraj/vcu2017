479f7953566426e1a77191b6bcf32f7c3cf7c759
tsselvaraj <timo.selvaraj@gmail.com>
2017-01-27 13:18:00 -0500

    Add files via upload

---
+++ textblob_classify.py
@@ -0,0 +1,24 @@
+# Please install textblob module
+
+from textblob import TextBlob
+from textblob.classifiers import NaiveBayesClassifier
+
+train = [
+    ('I love this sandwich.', 'pos'),
+    ('This is an amazing place!', 'pos'),
+    ('I feel very good about these beers.', 'pos'),
+    ('I do not like this restaurant', 'neg'),
+    ('I am tired of this stuff.', 'neg'),
+    ("I can't deal with this", 'neg'),
+    ("My boss is horrible.", "neg")
+    ]
+
+cl = NaiveBayesClassifier(train)
+
+print(cl.classify("I feel amazing!"))
+blob = TextBlob("The beer is good. But the hangover is horrible.", classifier=cl)
+
+for s in blob.sentences:
+     print(s)
+     print(s.classify())
+
